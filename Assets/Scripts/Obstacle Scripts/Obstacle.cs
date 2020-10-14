5cf445a46777fea0a64cf65b6ac4dcff33748419
Cedini <56426933+Cedini@users.noreply.github.com>
2020-10-14 14:00:54 +0300

    Progress

---
+++ Assets/Scripts/Obstacle Scripts/Obstacle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = -3f;
+    private Rigidbody2D myBody;
+    // Start is called before the first frame update
+    private void Awake() {
+        myBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myBody.velocity = new Vector2(speed, 0f);
+    }
+}
---
+++ Assets/Scripts/Player Scripts/PlayerJump.cs
@@ -0,0 +1,44 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerJump : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip jumpClip;
+    private Rigidbody2D myBody;
+    private float jumpForce = 12f, forwardForce = 0f;
+    private bool canJump;
+    private Button jumpBtn;
+    // Start is called before the first frame update
+    private void Awake() {
+        myBody = GetComponent<Rigidbody2D> ();
+        jumpBtn = GameObject.Find("Jump Button").GetComponent<Button> ();
+        jumpBtn.onClick.AddListener(() => Jump());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Mathf.Abs(myBody.velocity.y) == 0) {
+            canJump = true;
+        }
+    }
+
+    public void Jump() {
+        if (canJump) {
+            canJump = false;
+
+            //AudioSource.PlayClipAtPoint(jumpClip, transform.position);
+
+            if(transform.position.x < 0) {
+                forwardForce = 1f;
+            } else {
+                forwardForce = 0f;
+            }
+
+            myBody.velocity = new Vector2(forwardForce, jumpForce);
+        }
+    }
+}
